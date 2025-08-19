977b40ea5720d873f67355650c17ae0ccc281621
Andreas Berg <Andreas.Berg@iki.fi>
2025-08-19 19:00:13 +0300

    dev

--- src/app.ts
+++ src/app.ts
@@ -1,51 +1,51 @@
 import { defineOperationApp } from '@directus/extensions-sdk';
 
 export default defineOperationApp({
-	id: 'operation-slugify',
-	name: 'Slugify',
-	icon: 'box',
-	description: 'Generates language aware slugs.',
-	overview: ({ valueField, slugField, languageCodePath }) => [
-		{
-			label: 'Generates slugs based on',
-			text: valueField,
-		},
-		{
-			label: 'Name of the slug field',
-			text: slugField,
-		},
-		{
-			label: 'Language indicator',
-			text: languageCodePath,
-		},
-	],
-	options: [
-		{
-			field: 'valueField',
-			name: 'Name of the field to generate the slug from',
-			type: 'string',
-			meta: {
-				width: 'half',
-				interface: 'input',
-			},
-		},
-		{
-			field: 'languageCodePath',
-			name: 'Path to the language code or 2 letter language code',
-			type: 'string',
-			meta: {
-				width: 'half',
-				interface: 'input',
-			},
-		},
-		{
-			field: 'slugField',
-			name: 'Name of the slug field',
-			type: 'string',
-			meta: {
-				width: 'half',
-				interface: 'input',
-			},
-		},
-	],
-});
+        id: 'directus-extension-operation-nanoid-slug',
+        name: 'NanoID Slug',
+        icon: 'box',
+        description: 'Generates NanoID slugs.',
+        overview: ({ slugField, alphabet, length }) => [
+                {
+                        label: 'Name of the slug field',
+                        text: slugField,
+                },
+                {
+                        label: 'Custom alphabet (optional)',
+                        text: alphabet,
+                },
+                {
+                        label: 'Length (default = 21)',
+                        text: length,
+                },
+        ],
+        options: [
+                {
+                        field: 'slugField',
+                        name: 'Name of the slug field',
+                        type: 'string',
+                        meta: {
+                                width: 'half',
+                                interface: 'input',
+                        },
+                },
+                {
+                        field: 'alphabet',
+                        name: 'Custom alphabet',
+                        type: 'string',
+                        meta: {
+                                width: 'half',
+                                interface: 'input',
+                        },
+                },
+                {
+                        field: 'length',
+                        name: 'Length',
+                        type: 'number',
+                        meta: {
+                                width: 'half',
+                                interface: 'input',
+                        },
+                },
+        ],
+})
